05cf771c145e3b8a4d95b96e81745ef4049fb265
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-12-17 20:40:39 +0600

    Add project files.

---
+++ Restaurant POS System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurant_POS_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
