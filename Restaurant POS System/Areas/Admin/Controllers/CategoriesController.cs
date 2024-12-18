fac966d4f0edbb3218cbc97e35159742395c08d4
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-12-18 18:49:45 +0600

    Update Menu Category UI

--- Restaurant POS System/Areas/Admin/Controllers/CategoriesController.cs
+++ Restaurant POS System/Areas/Admin/Controllers/CategoriesController.cs
@@ -28,23 +28,7 @@ namespace Restaurant_POS_System.Areas.Admin.Controllers
                           Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Admin/Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+        
 
         // GET: Admin/Categories/Create
         public IActionResult Create()
